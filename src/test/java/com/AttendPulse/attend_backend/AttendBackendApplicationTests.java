54f764163e3db3996fca4f356312962e7f034ba4
BickyMallik <bickymallik1111@gmail.com>
2026-04-07 20:22:15 +0530

    chore: remove application.properties from git tracking

---
+++ src/test/java/com/AttendPulse/attend_backend/AttendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AttendPulse.attend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
