441d85e6f7962ddabb3301a10c849680dd59f3da
BickyMallik <bickymallik1111@gmail.com>
2026-04-07 19:58:34 +0530

    feat : add security layer - JWT, JwtFilter, SecurityConfig, UserDetailService

---
+++ src/main/java/com/AttendPulse/attend_backend/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.AttendPulse.attend_backend.security;
+
+import com.AttendPulse.attend_backend.entity.User;
+import com.AttendPulse.attend_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
